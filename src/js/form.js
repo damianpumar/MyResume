fde3f40bf100cf65826c6a71fdb6b5255e8eb2d2
Damián Pumar <damianpumar@gmail.com>
2019-08-03 00:09:18 +0200

    Improve css

--- src/js/form.js
+++ src/js/form.js
@@ -1,32 +1,34 @@
 export const form = {
-    applyValidators: () => {
-        $('#contact-form').addClass('validate-form');
-        $('#contact-form').find('input,textarea').each(function (index, element) {
-            if ($(this).attr('aria-required') === "true") {
-                $(this).addClass('required');
-            }
-            if ($(this).attr('name') === "email") {
-                $(this).addClass('email');
-            }
-        });
-
-        if ($('.validate-form').length) {
-            $('.validate-form').each(function () {
-                    $(this).validate({
-                    ignore: ".ignore",
-                    rules: {
-                        "hiddenRecaptcha": {
-                            required: function () {
-                                if (grecaptcha.getResponse() == '') {
-                                    return true;
-                                } else {
-                                    return false;
-                                }
-                            }
-                        }
-                    }
-                });
-            });
+  applyValidators: () => {
+    $("#contact-form").addClass("validate-form");
+    $("#contact-form")
+      .find("input,textarea")
+      .each(function(index, element) {
+        if ($(this).attr("aria-required") === "true") {
+          $(this).addClass("required");
         }
+        if ($(this).attr("name") === "email") {
+          $(this).addClass("email");
+        }
+      });
+
+    if ($(".validate-form").length) {
+      $(".validate-form").each(function() {
+        $(this).validate({
+          ignore: ".ignore",
+          rules: {
+            hiddenRecaptcha: {
+              required: function() {
+                if (grecaptcha.getResponse() == "") {
+                  return true;
+                } else {
+                  return false;
+                }
+              }
+            }
+          }
+        });
+      });
     }
-}
+  }
+};
