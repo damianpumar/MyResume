7691b99afbbb22230ae8a350779a432c3c6fe0e6
Damián Pumar <damianpumar@gmail.com>
2019-08-03 15:30:15 +0200

    Improve cookies

--- src/js/snackBar.js
+++ src/js/snackBar.js
@@ -5,29 +5,34 @@ export const snackBar = {
     $(window).load(function() {
       window.setTimeout(show, 2000);
     });
-
-    function show() {
-      // var snackBarWasShown = $.cookie("_sbSW");
-
-      // if (!snackBarWasShown) {
-      var snackbar = $("#snackbar");
-
-      // if (snackbar) {
-      snackbar.fadeIn("slow");
-
-      window.setTimeout(function() {
-        snackbar.fadeOut();
-      }, 6000);
-
-      $("#snackbar a").click(function(e) {
-        snackbar.hide();
-      });
-
-      var expireDate = new Date();
-
-      expireDate.setDate(expireDate.getDate() + 2);
-
-      // $.cookie("_sbSW", "true", { path: "/", expires: expireDate });
-    }
   }
 };
+function show() {
+  var snackBarWasShown = $.cookie("_sbSW");
+
+  if (snackBarWasShown) return;
+
+  var snackbar = $("#snackbar");
+
+  if (!snackbar) return;
+
+  snackbar.fadeIn("slow");
+
+  window.setTimeout(function() {
+    snackbar.fadeOut();
+  }, 6000);
+
+  hideWhenClicked(snackbar);
+}
+function hideWhenClicked(snackbar) {
+  $("#snackbar a").click(function(e) {
+    snackbar.hide();
+
+    saveCookies();
+  });
+}
+function saveCookies() {
+  var expireDate = new Date();
+  expireDate.setDate(expireDate.getDate() + 2);
+  $.cookie("_sbSW", "true", { path: "/", expires: expireDate });
+}
