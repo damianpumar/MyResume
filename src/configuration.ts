dd34539f86bad77d0758a145c476c65d065bf119
Damián Pumar <damianpumar@gmail.com>
2023-12-30 09:55:15 +0100

    ✨ Improve code

--- src/configuration.ts
+++ src/configuration.ts
@@ -7,15 +7,11 @@ export const personal = {
     "🏗️ Software Architect ~ 🧮 Software Engineer",
     "🥋 Software Craftsman ~ 🎤 Speaker",
   ],
-  bio: {
-    first: `
-    I'm a <span class="underline">Software Architect</span> and <span class="underline">Sr. software developer</span> with more than <span class="underline">13 years</span> of experience
-    in the industry I started to work professionally at 2010. I'm a passionate about software development and I love
-    sharing my knowledge with others. I'm a big fan of open source and I
-    contribute to different projects in my free time. I love to learn new things and I'm always looking for new challenges, mostly related to design patterns, dependency injection, software design with TDD and love write clean code.`,
-    second: `I started my career as a fullstack developer <a href="https://neoris.com/" class="underline" target="_blank">@Neoris</a> after that I worked as a Software Craftsman <a href="https://www.codurance.com/" class="underline" target="_blank">@Codurance</a>`,
-    third: `Today, I'm mainly focussed on frontend architecture designing components to make more easy, reusable and scalable the development of web applications like <a href="https://github.com/damianpumar/ts-injecty" class="underline" target="_blank">ts-injecty</a> and also as speaker in different conferences`,
-  },
+  bio: `I'm a <span class="underline">Software Architect</span> and <span class="underline">Sr. software developer</span> with more than <span class="underline">13 years</span> of experience
+    in the industry. I started to work professionally at 2010. I'm a passionate about software development and I love
+    sharing my knowledge with others.
+    I started my career as a fullstack developer <a href="https://neoris.com/" class="underline" target="_blank">@Neoris</a> after that I worked as a Software Craftsman <a href="https://www.codurance.com/" class="underline" target="_blank">@Codurance</a>.
+    Today, I'm mainly focussed on <span class="underline">frontend architecture</span> designing components to make more easy, reusable and scalable web applications like <a href="https://github.com/damianpumar/ts-injecty" class="underline" target="_blank">ts-injecty</a>. I'm also Speaker in different conferences and meetups.`,
   live: {
     where: "Spain",
     image: "🇪🇸",
@@ -24,22 +20,33 @@ export const personal = {
     where: "Argentina",
     image: "🇦🇷",
   },
-  doing: [
-    { name: "Ts-injecty", url: "https://github.com/damianpumar/ts-injecty" },
-    { name: "R2wc", url: "https://github.com/damianpumar/r2wc" },
-    { name: "Nuxt", url: "https://nuxt.com/" },
-    { name: "Node", url: "https://nodejs.org/" },
-    { name: "Typescript", url: "https://www.typescriptlang.org/" },
-    { name: "React", url: "https://reactjs.org/" },
-    { name: "Qwik", url: "https://qwik.builder.io/" },
-    { name: "Next", url: "https://nextjs.org/" },
-    { name: "Go", url: "https://go.dev/" },
-    { name: "Vue", url: "https://vuejs.org/" },
-    { name: "Azure", url: "https://azure.microsoft.com" },
-    { name: "Docker", url: "https://www.docker.com" },
-    { name: "Vite", url: "https://vitejs.dev/" },
-    { name: "Playwright", url: "https://playwright.dev/" },
-  ],
+  love: {
+    topics: [
+      { name: "Software Architecture" },
+      { name: "Design patterns" },
+      { name: "TDD" },
+    ],
+    technologies: [
+      { name: "Nuxt", url: "https://nuxt.com/" },
+      { name: "Node", url: "https://nodejs.org/" },
+      { name: "Typescript", url: "https://www.typescriptlang.org/" },
+      { name: "React", url: "https://reactjs.org/" },
+      { name: "Qwik", url: "https://qwik.builder.io/" },
+      { name: "Next", url: "https://nextjs.org/" },
+      { name: "Go", url: "https://go.dev/" },
+      { name: "Vue", url: "https://vuejs.org/" },
+      { name: "Azure", url: "https://azure.microsoft.com" },
+      { name: "Docker", url: "https://www.docker.com" },
+      { name: "Vite", url: "https://vitejs.dev/" },
+      { name: "Playwright", url: "https://playwright.dev/" },
+    ],
+    openSource: [
+      { name: "Argilla", url: "https://github.com/argilla-io/argilla" },
+      { name: "Ts-injecty", url: "https://github.com/damianpumar/ts-injecty" },
+      { name: "Svelte", url: "https://github.com/sveltejs/svelte" },
+      { name: "R2wc", url: "https://github.com/damianpumar/r2wc" },
+    ],
+  },
 };
 
 export const work = [
@@ -59,6 +66,7 @@ export const navigationItems = [
   { name: "Home", url: "/" },
   // { name: "Blog", url: "/blog/" },
   { name: "Events", url: "/events/" },
+  { name: "Contact", url: "mailto:pumar@duck.com" },
 ];
 
 export const social = [
@@ -71,6 +79,11 @@ export const social = [
     name: "Twitter",
     url: "https://twitter.com/damianpumar",
     icon: "i-uil-twitter",
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/damianpumar/",
+    icon: "i-uil-linkedin",
   },
   { name: "RSS", url: "/feed.xml", icon: "i-uil-rss" },
 ];
