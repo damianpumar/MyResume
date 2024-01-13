772358de356f10b3d96589cb961cef984ad4e06f
Damián Pumar <damianpumar@gmail.com>
2024-01-13 18:58:54 +0100

    ✨ Improve fonts

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import UnoCSS from "unocss/astro";
-import { FontaineTransform } from "fontaine";
 
 export default defineConfig({
   site: "https://www.damianpumar.com/",
@@ -14,11 +13,5 @@ export default defineConfig({
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
     },
-    plugins: [
-      FontaineTransform.vite({
-        fallbacks: ["Arial"],
-        resolvePath: (id) => new URL(`./public${id}`, import.meta.url), // id is the font src value in the CSS
-      }),
-    ],
   },
 });
